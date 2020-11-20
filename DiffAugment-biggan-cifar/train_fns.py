105b1cf68977e18dc8dbabc5110a1447aa76babd
cesar-claros <65795140+cesar-claros@users.noreply.github.com>
2020-11-20 11:52:03 -0400

    inception codes output

--- DiffAugment-biggan-cifar/train_fns.py
+++ DiffAugment-biggan-cifar/train_fns.py
@@ -276,10 +276,10 @@ def eval(G, D, GD, G_ema, z_, y_, state_dict, config, sample, get_inception_metr
     folder_fake = '%s/%s/%d' % (config['samples_root'], experiment_name,
                                                     state_dict['itr'])
     os.mkdir(folder_fake)
-    IS_mean, IS_std, FID, sigma_fake = get_inception_metrics(sample,
+    IS_mean, IS_std, FID, sigma_fake, pool = get_inception_metrics(sample,
                                                  config['num_inception_images'],
                                                  folder_fake,
                                                  num_splits=10)
     print('Itr %d: PYTORCH UNOFFICIAL Inception Score is %3.3f +/- %3.3f, PYTORCH UNOFFICIAL FID is %5.4f' %
           (state_dict['itr'], IS_mean, IS_std, FID))
-    return IS_mean, IS_std, FID, sigma_fake 
+    return IS_mean, IS_std, FID, sigma_fake, pool 
